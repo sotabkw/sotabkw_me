6544d3489724656600f7d0e2da0d41bc774dab89
watanabe-sota <819sota@gmail.com>
2022-02-13 07:34:38 +0900

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const withOptimizedImages = require('next-optimized-images')
-
 module.exports = {
-  // https://github.com/vercel/next.js/issues/21079
-  // Remove this workaround whenever the issue is fixed
   images: {
     disableStaticImages: true,
   },
 }
-
-module.exports = withOptimizedImages({
-  handleImages: ['jpeg', 'png', 'svg'],
-})
