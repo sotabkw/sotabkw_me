fcb23aad2244bf9dc968a60fc08c1fd59feb4487
watanabe-sota <819sota@gmail.com>
2022-02-13 05:26:37 +0900

    add: constant

---
+++ src/components/constant/SkillContent.ts
@@ -0,0 +1,49 @@
+export const SkillContents = [
+  {
+    name: 'JavaScript',
+    href: 'https://developer.mozilla.org/ja/docs/Web/JavaScript',
+  },
+  {
+    name: 'TypeScript',
+    href: 'https://www.typescriptlang.org/',
+  },
+  {
+    name: 'Next.js',
+    href: 'https://nextjs.org/',
+  },
+  {
+    name: 'React.js',
+    href: 'https://reactjs.org/',
+  },
+  {
+    name: 'HTML5',
+    href: 'http://www.htmq.com/html5/',
+  },
+  {
+    name: 'CSS',
+    href: 'https://developer.mozilla.org/ja/docs/Web/CSS/Reference',
+  },
+  {
+    name: 'Swift',
+    href: 'https://docs.swift.org/swift-book/index.html',
+  },
+  {
+    name: 'Go',
+    href: 'https://go.dev/',
+  },
+  {
+    name: 'Atomic Design',
+  },
+  {
+    name: 'DDD',
+  },
+  {
+    name: 'Clean architecture',
+  },
+  {
+    name: 'Git',
+  },
+  {
+    name: 'Github Action',
+  },
+]
