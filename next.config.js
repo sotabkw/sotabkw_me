8179db3ea98f68d92196564ba0f19f3e8bd711a8
watanabe-sota <819sota@gmail.com>
2022-02-13 06:57:00 +0900

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   // https://github.com/vercel/next.js/issues/21079
   // Remove this workaround whenever the issue is fixed
   images: {
-    loader: 'imgix',
-    path: '',
+    disableStaticImages: true,
   },
 }
