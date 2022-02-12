8b93a479e455e99c1518658888170a37e2ab7ce1
watanabe-sota <819sota@gmail.com>
2022-02-12 22:54:15 +0900

    fix: lint error

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -20,12 +20,12 @@ export default function FourZeroFour() {
         </div>
         <div className="max-w-md">
           <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
+            Sorry we couldn&apos;t find this page.
           </p>
           <p className="mb-8">
             But dont worry, you can find plenty of other things on our homepage.
           </p>
-          <Link href="/">
+          <Link href="/" passHref>
             <Button
               as="a"
               className="focus:shadow-outline-primary inline rounded-lg border border-transparent bg-primary-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-primary-700 focus:outline-none dark:hover:bg-primary-500"
