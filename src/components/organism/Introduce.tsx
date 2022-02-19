b776c51b5321216f80f0cca51f055b69086ce775
watanabe-sota <819sota@gmail.com>
2022-02-19 20:09:13 +0900

    fix: layout

--- src/components/organism/Introduce.tsx
+++ src/components/organism/Introduce.tsx
@@ -8,10 +8,10 @@ export const Introduce: React.VFC = () => {
         <div className="mr-2 flex items-center">
           <ChevronRight />
         </div>
-        Introduce
+        about me
       </h1>
       <p className="text-lg font-sans text-gray-500 dark:text-gray-100">
-        Hi, my name is Sota. Welcome to my site. I&aposm a Software Engineer. I
+        Hi, my name is Sota. Welcome to my site. I&apos;m a Software Engineer. I
         like the front-end, so I want to pursue technical pursuits and at the
         same time pursue how to deliver excellent UI/UX to users as a product.
         However, I am also interested in the back-end and am currently studying.
