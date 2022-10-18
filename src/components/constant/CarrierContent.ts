3cd9be91def0fac2d9873442ee415ffe94fce05d
watanabe-sota <819sota@gmail.com>
2022-10-18 12:46:27 +0900

    fix: carrerContent

--- src/components/constant/CarrierContent.ts
+++ src/components/constant/CarrierContent.ts
@@ -28,4 +28,11 @@ export const CareerContents = [
     incHref:
       'https://www.camel-delivery.com/?utm_term=%2Btacoms&utm_campaign=KT_%E3%83%96%E3%83%A9%E3%83%B3%E3%83%89%E6%8C%87%E5%90%8D&utm_source=adwords&utm_medium=ppc&hsa_acc=5543580488&hsa_cam=12937585434&hsa_grp=128572927904&hsa_ad=521834286629&hsa_src=g&hsa_tgt=kwd-678200243707&hsa_kw=%2Btacoms&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gclid=CjwKCAjwz5iMBhAEEiwAMEAwGIGEraaAnJP3srRSELX63DoJ4H53lp_Bgl16Qge4KFD48IHICH9ZMBoCGOYQAvD_BwE',
   },
+  {
+    title: 'LabBase Inc',
+    date: '2021.10 - present',
+    body: 'Web front-end internship in LabBase',
+    isPresent: false,
+    incHref: 'https://labbase.jp/',
+  },
 ]
