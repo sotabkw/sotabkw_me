d891862f5cbfced90b68e2ed275744087c7c7b94
sotabkw <819sota@gmail.com>
2024-12-07 02:11:59 +0900

    updated_20241207

--- src/components/organism/Introduce.tsx
+++ src/components/organism/Introduce.tsx
@@ -11,10 +11,8 @@ export const Introduce: React.VFC = () => {
         About me
       </h1>
       <p className="text-lg font-sans text-gray-500 dark:text-gray-100">
-        Hi, my name is Sota. Welcome to my site. I&apos;m a Software Engineer. I
-        like the front-end, so I want to pursue technical pursuits and at the
-        same time pursue how to deliver excellent UI/UX to users as a product.
-        However, I am also interested in the back-end and am currently studying.
+        ソフトウェアエンジニアしています。
+        エンジニアとしては、フロントエンドやサーバーサイドにおけるアプリケーションの開発経験が多いです。
       </p>
     </div>
   )
